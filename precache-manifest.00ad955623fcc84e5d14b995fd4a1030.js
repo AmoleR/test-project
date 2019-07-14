5a86ffe397c50104bec66410695020669573067f
AmoleR <43121460+AmoleR@users.noreply.github.com>
2019-07-13 20:31:41 -0700

    Updates

---
+++ precache-manifest.00ad955623fcc84e5d14b995fd4a1030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058d11e97ef3518ce74ebdb74bdde1fa",
+    "url": "/test-project/index.html"
+  },
+  {
+    "revision": "c390349b29fb9d2a99ed",
+    "url": "/test-project/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test-project/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c390349b29fb9d2a99ed",
+    "url": "/test-project/static/js/main.52cabc14.chunk.js"
+  },
+  {
+    "revision": "8c473920f916ba8f9ddf",
+    "url": "/test-project/static/js/runtime~main.8e57902c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
