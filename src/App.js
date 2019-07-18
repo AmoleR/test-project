81ed4cd3e37bb61f2c6fd418c6cfff46d4dda6fa
AmoleR <43121460+AmoleR@users.noreply.github.com>
2019-07-17 19:39:40 -0700

    HashRouter

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import CatanGame from './catanGame.js';
 import HowToPlay from './howToPlay.js';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Route exact path="/play" component={CatanGame} />
-        <Route exact path="/" component={HowToPlay}  />
-      </Router>
+        <HashRouter basename='/'>
+          <Route path='/play' component={CatanGame} />
+          <Route exact path='/' component={HowToPlay}  />
+        </HashRouter>
     );
   }
 }
